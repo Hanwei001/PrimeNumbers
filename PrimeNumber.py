96d3557b6ae051355e0b288d4d80f57fd6c05f44
Hanwei001 <hanwei.li@gmx.de>
2026-01-07 15:06:26 +0100

    final version

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,5 +1,15 @@
-def Main():
+def main():
+    with open("results.txt", "w", encoding="utf-8") as f:
+        for number in range(2, 251):
+            flag = True
 
+            for div in range(2, number):
+                if number % div == 0:
+                    flag = False
+                    break
+
+            if flag:
+                f.write(str(number) + "\n")
 
 if __name__ == "__main__":
     main()
